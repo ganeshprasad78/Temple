f45b40f84eccf4a1afe105898857c441dc4ef840
Ganesh <ganesh@ubuntu.ubuntu-domain>
2014-08-25 14:36:26 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Temple::Application.config.secret_key_base = '9bd90f15529bf1cbca1d565f55d13b1c6a7bc11f6485903965d5d7a49ecd98d76dfbbfb45ecfae6ff3d39491cd65d8c1648be03b64556c266addf4fbeeee8ab3'
